2484ca74c82eb902dc22150f443a9c3d6217af3b
Dominic <self103@mail.chapman.edu>
2015-12-16 18:31:43 -0800

    added new resources

--- js/resources.js
+++ js/resources.js
@@ -3,7 +3,7 @@ game.resources = [
     {name: "bg", type:"image", src: "data/img/chaptown.png"},
     {name: "clumsy", type:"image", src: "data/img/panther.png"},
     {name: "pipe", type:"image", src: "data/img/pipe.png"},
-    {name: "logo", type:"image", src: "data/img/pantherlogo.png"},
+    {name: "logo", type:"image", src: "data/img/logoPanther.png"},
     {name: "ground", type:"image", src: "data/img/ground.png"},
     {name: "gameover", type:"image", src: "data/img/gameover.png"},
     {name: "gameoverbg", type:"image", src: "data/img/gameoverbg.png"},
@@ -12,6 +12,12 @@ game.resources = [
     {name: "new", type:"image", src: "data/img/new.png"},
     {name: "share", type:"image", src: "data/img/share.png"},
     {name: "tweet", type:"image", src: "data/img/tweet.png"},
+    {name: "windowOfOpp", type:"image", src: "data/img/cu_window.png"},
+    {name: "readytochap", type:"image", src: "data/img/readytochap.png"},
+    {name: "pipe1", type:"image", src: "data/img/pipe1.png"},
+    {name: "pipe2", type:"image", src: "data/img/pipe2.png"},
+    {name: "pipe3", type:"image", src: "data/img/pipe3.png"},
+
     // sounds
     {name: "theme", type: "audio", src: "data/bgm/"},
     {name: "hit", type: "audio", src: "data/sfx/"},
